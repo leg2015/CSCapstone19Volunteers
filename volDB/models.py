aafab1ba08b1941b6f1fd26fd3d7702a73c034b3
S Boyd <boyds@southwestern.edu>
2019-02-23 11:05:46 -0600

    main DB table created

--- volDB/models.py
+++ volDB/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Organization(models.Model):
+    name = models.CharField(db_colums='orgName', max_length = 100, blank=False)
+    orgID = models.AutoField(db_colums='orgID', primary_key=True)
+    mission = models.TextField(db_colums='missionStatement')
+    opportunities = models.TextField(db_colums='volOpportunities')
+    website = models.URLField(db_colums='volURL', blank=False)
+    notes = models.TextField(db_colums='notes')
