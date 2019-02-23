d750a31e972cb5eb4645511c6f49331b00381311
S Boyd <boyds@southwestern.edu>
2019-02-23 11:46:07 -0600

    added locations, category, address, and email tables to models.py

--- volDB/models.py
+++ volDB/models.py
@@ -11,6 +11,29 @@ class Organization(models.Model):
 
 
 class Phone(models.Model):
-    phoneID = models.AutoField(db_column = 'phoneID', primary_key = true)
+    phoneID = models.AutoField(db_column = 'phoneID', primary_key = True)
     phone = models.CharField(db_column = 'phone', max_length = 10) #10 digit phone number, no dashes or spaces
     orgid = models.ForeignKey(Organization, models.DO_NOTHING, db_column = 'orgID')
+
+class Category(models.Model):
+    orgID = models.ForeignKey(Organization, models.DO_NOTHING, db_column="orgID")
+    category = models.CharField(db_column='category', max_length=20)
+    categoryID = models.AutoField(db_column = 'categoryID', primary_key = True)
+
+class Email(models.Model):
+    orgID = models.ForeignKey(Organization, models.DO_NOTHING, db_column="orgID")
+    email = models.EmailField(db_column='email', max_length=254)
+    emailID = models.AutoField(db_column = 'emailID', primary_key = True)
+
+class Location(models.Model):
+    orgID = models.ForeignKey(Organization, models.DO_NOTHING, db_column="orgID")
+    location = models.CharField(db_column='location', max_length=20)
+    locationID = models.AutoField(db_column = 'locationID', primary_key = True)
+
+class Address(models.Model):
+    orgID = models.ForeignKey(Organization, models.DO_NOTHING, db_column="orgID")
+    addressID = models.AutoField(db_column = 'addressID', primary_key = True)
+    street = models.CharField(db_column='street', max_length=100)
+    city = models.CharField(db_column='city', max_length=20)
+    state = models.CharField(db_column='state')
+    zipCode = models.IntegerField(db_column='zipCode', max_length=50)
