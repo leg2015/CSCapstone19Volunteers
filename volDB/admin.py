9da1d660f0e354066c1cce7a6a62497ddc1ac419
S Boyd <boyds@southwestern.edu>
2019-02-24 11:04:22 -0600

    Admin can now edit all tables from the same page and not required fields are enforced

--- volDB/admin.py
+++ volDB/admin.py
@@ -2,9 +2,28 @@ from django.contrib import admin
 from .models import Organization, Phone, Category, Email, Location, Address
 
 # Register your models here.
-admin.site.register(Organization)
-admin.site.register(Phone)
-admin.site.register(Category)
-admin.site.register(Email)
-admin.site.register(Location)
-admin.site.register(Address)
+
+# For registering each table separately
+# admin.site.register(Organization)
+# admin.site.register(Phone)
+# admin.site.register(Category)
+# admin.site.register(Email)
+# admin.site.register(Location)
+# admin.site.register(Address)
+
+# For registering all tables on same admin page
+# https://docs.djangoproject.com/en/1.8/ref/contrib/admin/#django.contrib.admin.TabularInline
+class PhoneAdmin(admin.TabularInline):
+    model = Phone
+class CategoryAdmin(admin.TabularInline):
+    model = Category
+class LocationAdmin(admin.TabularInline):
+    model = Location 
+class EmailAdmin(admin.TabularInline):
+    model = Email  
+class AddressAdmin(admin.TabularInline):
+    model = Address
+class OrganizationAdmin(admin.ModelAdmin):
+    inlines = [CategoryAdmin, LocationAdmin, PhoneAdmin, EmailAdmin, AddressAdmin]
+
+admin.site.register(Organization, OrganizationAdmin)
