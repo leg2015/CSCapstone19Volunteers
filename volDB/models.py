1ee28fabf4475e801dab0fd137b23661ec6cc96a
S Boyd <boyds@southwestern.edu>
2019-02-25 19:26:33 -0600

    added admin filters and multivariables for locations and categories

--- volDB/models.py
+++ volDB/models.py
@@ -1,6 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category = models.CharField(db_column='category', max_length=20)
+    categoryID = models.AutoField(db_column = 'categoryID', primary_key = True)
+
+    def __str__(self):
+        return self.category
+    class Meta:
+        verbose_name_plural = "Categories"
+
+class Location(models.Model):
+    location = models.CharField(db_column='location', max_length=20)
+    locationID = models.AutoField(db_column = 'locationID', primary_key = True)
+
+    def __str__(self):
+        return self.location
+    class Meta:
+        verbose_name_plural = "Locations"
+
 class Organization(models.Model):
     name = models.CharField(db_column='orgName', max_length = 100, blank=False)
     orgID = models.AutoField(db_column='orgID', primary_key=True)
@@ -8,29 +26,21 @@ class Organization(models.Model):
     opportunities = models.TextField(db_column='volOpportunities', blank=True)
     website = models.URLField(db_column='volURL', blank=False)
     notes = models.TextField(db_column='notes', blank=True)
+    isVisible = models.BooleanField(db_column='visible', default=True)
+    category = models.ManyToManyField(Category, db_column='category', blank=True)
+    location = models.ManyToManyField(Location, db_column='location', blank=True)
     def __str__(self):
         return self.name
-
 
 class Phone(models.Model):
     phoneID = models.AutoField(db_column = 'phoneID', primary_key = True)
     phone = models.CharField(db_column = 'phone', max_length = 10) #10 digit phone number, no dashes or spaces
     orgid = models.ForeignKey(Organization, models.DO_NOTHING, db_column = 'orgID')
 
-class Category(models.Model):
-    orgID = models.ForeignKey(Organization, models.DO_NOTHING, db_column="orgID")
-    category = models.CharField(db_column='category', max_length=20)
-    categoryID = models.AutoField(db_column = 'categoryID', primary_key = True)
-
 class Email(models.Model):
     orgID = models.ForeignKey(Organization, models.DO_NOTHING, db_column="orgID")
     email = models.EmailField(db_column='email', max_length=254)
     emailID = models.AutoField(db_column = 'emailID', primary_key = True)
-
-class Location(models.Model):
-    orgID = models.ForeignKey(Organization, models.DO_NOTHING, db_column="orgID")
-    location = models.CharField(db_column='location', max_length=20)
-    locationID = models.AutoField(db_column = 'locationID', primary_key = True)
 
 class Address(models.Model):
     orgID = models.ForeignKey(Organization, models.DO_NOTHING, db_column="orgID")
