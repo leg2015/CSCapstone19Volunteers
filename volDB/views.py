3252a7c0935db887b74c0e50a9f6f29e3af2f842
Colin Scruggs <colinscruggs97@gmail.com>
2019-02-27 18:18:19 -0600

    Created static folder, added QuerySets for categories/locations in index

--- volDB/views.py
+++ volDB/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Category
+from .models import Location
+from .models import Organization
 
 # index view: will render index.html upon request
+
 def index(request):
-    return render(request, 'index.html', {})
+    categories = Category.objects.order_by('category') # create QuerySet with all categories in volDB
+    locations = Location.objects.all() # create QuerySet with all locations in volDB
+    return render(request, 'index.html', {'categories': categories, 'locations': locations})
+
+# TODO: figure out how to properly nest index template into base.html and use this method
+def base(request):
+    categories = Category.objects.order_by('category') # create QuerySet with all categories in volDB
+    locations = Location.objects.all() # create QuerySet with all locations in volDB
+    return render(request, 'base.html', {'categories': categories, 'locations': locations})
