a7930f3a2a5da8833fe4cbaca2c330da055d9351
Colin Scruggs <colinscruggs97@gmail.com>
2019-03-12 20:11:50 -0500

    Django form and POST to results.html

---
+++ volDB/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Category
+from .models import Location
+from .models import Organization
+
+# TODO: figure out initial values 
+class LandingPageForm(forms.Form):
+    category = forms.ModelChoiceField(queryset=Category.objects.order_by('category'), 
+                                            required=False, 
+                                            initial="Any", 
+                                            widget=forms.Select)
+    location = forms.ModelChoiceField(queryset=Location.objects.all(), 
+                                    required=False, 
+                                    initial="Georgetown",
+                                    widget=forms.Select)
+  
--- volDB/views.py
+++ volDB/views.py
@@ -3,18 +3,45 @@ from django.http import HttpResponse
 from .models import Category
 from .models import Location
 from .models import Organization
+from .forms import LandingPageForm
 
 # index view: will render index.html upon request
 
 def index(request):
     categories = Category.objects.order_by('category') # create QuerySet with all categories in volDB
     locations = Location.objects.all() # create QuerySet with all locations in volDB
-    return render(request, 'index.html', {'categories': categories, 'locations': locations})
+    form = LandingPageForm()
+    return render(request, 'index.html', {'categories': categories, 'locations': locations, 'form': form})
 
 def results(request):
-    categories = Category.objects.order_by('category') # create QuerySet with all categories in volDB
-    locations = Location.objects.all() # create QuerySet with all locations in volDB
-    return render(request, 'results.html', {'categories': categories, 'locations': locations})
+
+    form = LandingPageForm(request.POST)
+    if form.is_valid():
+        form_data = form.cleaned_data
+        print(form_data)
+
+    print(form_data['category'])
+    print(form_data['location'])
+
+    results = Organization.objects.all().filter(
+        location=form_data['location']
+    ).filter(
+        category=form_data['category']
+    )
+
+    #results = Organization.objects.all() 
+
+    args = {
+        'form': form,
+        'results': results
+    }
+
+    return render(request, 'results.html', args)
+
+# def results(request):
+#     categories = Category.objects.order_by('category') # create QuerySet with all categories in volDB
+#     locations = Location.objects.all() # create QuerySet with all locations in volDB
+#     return render(request, 'results.html', {'categories': categories, 'locations': locations})
 
 # TODO: figure out how to properly nest index template into base.html and use this method
 # def base(request):
