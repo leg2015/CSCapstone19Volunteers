a06949d28da5b7cafeaa1f82dd4cdb42f2f5c404
Colin Scruggs <colinscruggs97@gmail.com>
2019-03-12 22:15:30 -0500

    Added conditionally rendered cards to results UI

--- volDB/forms.py
+++ volDB/forms.py
@@ -8,9 +8,10 @@ class LandingPageForm(forms.Form):
     category = forms.ModelChoiceField(queryset=Category.objects.order_by('category'), 
                                             required=False, 
                                             initial="Any", 
-                                            widget=forms.Select)
+                                            widget=forms.Select(attrs={'class' : 'form-control'})
+                                            )
     location = forms.ModelChoiceField(queryset=Location.objects.all(), 
                                     required=False, 
                                     initial="Georgetown",
-                                    widget=forms.Select)
+                                    widget=forms.Select(attrs={'class' : 'form-control'}))
   
