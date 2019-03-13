62eabccebcc0fc712c49127e4faf4d68ad59588d
Colin Scruggs <colinscruggs97@gmail.com>
2019-03-12 22:15:30 -0500

    Added conditionally rendered cards to results UI

--- volDB/views.py
+++ volDB/views.py
@@ -6,42 +6,37 @@ from .models import Organization
 from .forms import LandingPageForm
 
 # index view: will render index.html upon request
-
 def index(request):
     categories = Category.objects.order_by('category') # create QuerySet with all categories in volDB
     locations = Location.objects.all() # create QuerySet with all locations in volDB
-    form = LandingPageForm()
+    form = LandingPageForm() # default form
     return render(request, 'index.html', {'categories': categories, 'locations': locations, 'form': form})
 
+# results view: takes POST request to render results page with data from landing page form
 def results(request):
-
-    form = LandingPageForm(request.POST)
+    form = LandingPageForm(request.POST) # assign form to POST request of data in LandingPageForm
     if form.is_valid():
         form_data = form.cleaned_data
         print(form_data)
 
-    print(form_data['category'])
-    print(form_data['location'])
-
+    # filter organizations based on location and category chosen on landing page form
     results = Organization.objects.all().filter(
         location=form_data['location']
     ).filter(
         category=form_data['category']
     )
 
+    # Uncomment to show all organizations in database
     #results = Organization.objects.all() 
 
+    # create arguments dict that holds the form and filtered results to pass to 
     args = {
         'form': form,
         'results': results
     }
 
-    return render(request, 'results.html', args)
-
-# def results(request):
-#     categories = Category.objects.order_by('category') # create QuerySet with all categories in volDB
-#     locations = Location.objects.all() # create QuerySet with all locations in volDB
-#     return render(request, 'results.html', {'categories': categories, 'locations': locations})
+    # render request: uses organizationCards.html (which extends results.html)
+    return render(request, 'organizationCards.html', args)
 
 # TODO: figure out how to properly nest index template into base.html and use this method
 # def base(request):
