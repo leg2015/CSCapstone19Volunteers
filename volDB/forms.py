ecf417324222ceb351a6f2ba5dbe61b1c9d7ad5e
S Boyd <boyds@southwestern.edu>
2019-04-01 16:47:30 -0500

    can filter by category and location and can search specific location with Places API autocorrect

--- volDB/forms.py
+++ volDB/forms.py
@@ -14,4 +14,19 @@ class LandingPageForm(forms.Form):
                                     required=False, 
                                     initial="Georgetown",
                                     widget=forms.Select(attrs={'class' : 'form-control'}))
+    myLocation = forms.CharField(max_length=100,widget=forms.TextInput(attrs={'class': 'form-control', 'id': 'searchLocation'}), required=False)
+    radius = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control'}), required=False)
+# class ResultsPageForm(forms.Form):
+#     category = forms.ModelChoiceField(queryset=Category.objects.order_by('category'), 
+#                                             required=False, 
+#                                             initial="Any", 
+#                                             widget=forms.Select(attrs={'class' : 'form-control'})
+#                                             )
+#     location = forms.ModelChoiceField(queryset=Location.objects.all(), 
+#                                     required=False, 
+#                                     initial="Georgetown",
+#                                     widget=forms.Select(attrs={'class' : 'form-control'}))
+#     myLocation = forms.CharField(max_length=100,widget=forms.TextInput(attrs={'class': 'searchLocation'}))
+#     radius = forms.IntegerField()
+    #input type="text" class="form-group" name="location" id="searchLocation"
   
