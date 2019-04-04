b410627cfff35f65b87cd94af6f8e16f3121f5aa
S Boyd <boyds@southwestern.edu>
2019-04-04 15:40:19 -0500

    added capability for multiple forms

--- volDB/views.py
+++ volDB/views.py
@@ -27,40 +27,46 @@ def queryRadius(Queryset, addresses, radius):
 
 # results view: takes POST request to render results page with data from landing page form
 def results(request):
-    indexForm = LandingPageForm(request.POST) # assign form to POST request of data in LandingPageForm
+    if 'refineSearch' in request.POST:
+        indexForm = filterForm(request.POST)
+    else:
+        indexForm = LandingPageForm(request.POST) # assign form to POST request of data in LandingPageForm
+    
     if indexForm.is_valid():
         form_data = indexForm.cleaned_data
-    keys = form_data.keys()
-    results = Organization.objects.all()
-    if form_data['location'] != None:
-        results = queryLocation(results, form_data['location'])
-    if form_data['category'] != None:
-        results = queryCategory(results, form_data['category'])
-    # Uncomment to show all organizations in database
-    # results = Organization.objects.all() 
+        keys = form_data.keys()
+        results = Organization.objects.all()
+        if 'location' in keys:
+            if form_data['location'] != None:
+                results = queryLocation(results, form_data['location'])
+        if form_data['category'] != None:
+            results = queryCategory(results, form_data['category'])
+        # Uncomment to show all organizations in database
+        # results = Organization.objects.all() 
 
-    # create list with all orginzation IDs found in filtered results
-    results_orgIDs = [result.orgID for result in results]
+        # create list with all orginzation IDs found in filtered results
+        results_orgIDs = [result.orgID for result in results]
 
-    # made a QuerySet of Address objects filtered from the orgIDs in the above list
-    addresses = Address.objects.filter(orgID__in=results_orgIDs)
-    if 'myLocation' in keys:
-        if form_data['myLocation'] != None:
-            radius = 50
-            if form_data['radius'] != None:
-                radius = form_data['radius']
-            results, addresses = queryRadius(results, addresses, radius)
+        # made a QuerySet of Address objects filtered from the orgIDs in the above list
+        addresses = Address.objects.filter(orgID__in=results_orgIDs)
+        if 'myLocation' in keys:
+            print(form_data['myLocation'])
+            if form_data['myLocation'] != None:
+                radius = 50
+                if form_data['radius'] != None:
+                    radius = form_data['radius']
+                results, addresses = queryRadius(results, addresses, radius)
 
-    # zip together results and addresses to pass to results.html 
-    resultsWithAddresses = zip(results, addresses)
+        # zip together results and addresses to pass to results.html 
+        resultsWithAddresses = zip(results, addresses)
 
-    # create arguments dict that holds the form and filtered results to pass to 
-    args = {
-        'filterForm': filterForm,
-        'results': results,
-        'addresses': addresses,
-        'resultsWithAddresses': resultsWithAddresses
-    }
+        # create arguments dict that holds the form and filtered results to pass to 
+        args = {
+            'filterForm': filterForm,
+            'results': results,
+            'addresses': addresses,
+            'resultsWithAddresses': resultsWithAddresses
+        }
 
-    # render request: uses organizationCards.html (which extends results.html)
-    return render(request, 'organizationCards.html', args)
+        # render request: uses organizationCards.html (which extends results.html)
+        return render(request, 'organizationCards.html', args)
