18ffa6825775b9473956ce0826ff04c8c7dc2422
S Boyd <boyds@southwestern.edu>
2019-04-14 14:34:43 -0500

    logout page that matches styling of home page

--- volDB/urls.py
+++ volDB/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url
 from . import views
 
 # url patterns list:
-# currently contains index and results page
+# currently contains index, results, and logout page
 urlpatterns = [
   url(r'^$', views.index, name = 'index'),
-  url('results', views.results, name = 'results') # create url path to results page
+  url('results', views.results, name = 'results'), # create url path to results page
+  url(r'^logout/$', views.logout, name='logout') # creatue url path to logout page
 ]
