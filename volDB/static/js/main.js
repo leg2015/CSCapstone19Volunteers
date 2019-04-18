d784d62fc73f5c87eb8b49c8e0763d452ab41104
Colin Scruggs <colinscruggs97@gmail.com>
2019-04-18 15:06:37 -0500

    Async access geocode API and draw markers based on results

--- volDB/static/js/main.js
+++ volDB/static/js/main.js
@@ -1,23 +1,3 @@
-// Based on this tutorial: https://developers.google.com/maps/documentation/javascript/adding-a-google-map#get_latLng
-// Initialize and add the map
-function initMap() {
-  // The location of Uluru
-  var georgetown = {
-    lat: 30.6349,
-    lng: -97.6651
-  };
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-    document.getElementById('map'), {
-      zoom: 10,
-      center: georgetown
-    });
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({
-    position: georgetown,
-    map: map
-  });
-}
 
 /* Landing page form replacement */
 
@@ -28,28 +8,104 @@ $( document ).ready(function() {
   $("#id_location option:first-child").text("Any Location");
 });
 
-/* Address logic  */
+/* Google Map API initialization */
 
-var jsonData = [];
+var map; // create global 'map' variable to be used throughout main.js
 
-// Grab data from serialized json object (passed in base.html) 
-function jsonPass(data){
-  data.forEach(function(address) {
-    jsonData.push({'orgId':address.fields.orgID, 'fields':address.fields,
-                   'address': address.fields.street + ', ' + address.fields.city + ', ' + 
-                              address.fields.state + ' ' + address.fields.zipCode})
-  })
-  console.log(jsonData);
+// Based on this tutorial: https://developers.google.com/maps/documentation/javascript/adding-a-google-map#get_latLng
+// Initialize and add the map (callback method used in maps API cdn in base.html)
+async function initMap() {
+  // The location of Georgetown; TODO: pass in selected location (city) to center map near results
+  var georgetown = {
+    lat: 30.6349,
+    lng: -97.6651
+  };
+  // The map, centered at given location
+  map = new google.maps.Map(
+    document.getElementById('map'), {
+      zoom: 10,
+      center: georgetown
+    });
 
-  return data;
+   await drawMarkers();
 }
 
-jsonData.forEach(function(addressObj) {
-  var addressString = addressObj.address;
-  //var geocode = https://maps.googleapis.com/maps/api/geocode/json?address=addressString&key=AIzaSyDXKsysbYwfZ89T85u9Na24a0wlUMG7Txg;
+/* Address logic  */
+
+// Asyncronously recieve geocoding information from API (using Axios library) with a given address string
+async function geocode(addressStr) {
+  await axios.get('https://maps.googleapis.com/maps/api/geocode/json?', {
+    params: {
+      address: addressStr,
+      key:'AIzaSyDXKsysbYwfZ89T85u9Na24a0wlUMG7Txg' // TODO: !!! HIDE THIS KEY BEFORE DISTRIBUTION BUILD !!!
+    }
+  }).then(response => {
+    // log full response
+    // console.log(response);
+    drawMarker(response);
+    // TODO: pass organization name along with json data for adding labels to markers
+
+    return response;
+  }).catch(error => {
+    console.log("Google Maps Geocoding API error: " + error);
+  })
+}
+
+// drawMarker: asyncronously draw a marker on map after waiting for the geocode method to fetch data
+async function drawMarker(response) {
+  // console.log("output from drawMarker function:");
+  // console.log(response);
+
+  // grab latitude and longitude 
+  var lat = response.data.results[0].geometry.location.lat;
+  var lng = response.data.results[0].geometry.location.lng;
+  var formatted_address = response.data.results[0].formatted_address;
+  // console.log("Rendering pin at latitude: " + lat + " | longitude: " + lng);
+
+  // create a new marker 
   var marker = new google.maps.Marker({
-    position: georgetown,
-    title:addressObj.address
+    
+    position: { lat: lat, lng: lng},
+    map: map,
+    title: formatted_address
   });
-  console.log(geocode);
-})
+}
+
+// old attempt: keeping for one commit (TODO: delete this before next commit)
+// function drawMarkers() {
+
+//   jsonData.forEach(function(addressInfo) {
+//     // access each address object's formatted address field from database
+//     var addressString = addressInfo.address;
+//     // console.log(addressString);
+    
+//     // declare addressInfo to be assigned output from geocode Promise
+//     var addressObj;
+//     addressObj = geocode(addressString).then(response => {
+//       console.log("result from geocode: " + response);
+//       // console.log("addressObj after assignment: " + addressObj);
+//       return response;
+//       response.json({ message: 'Request recieved in drawmMarkers!', data})
+//       console.log(response.json({ message: 'Request recieved in drawmMarkers!', data}));
+//       addressObj = result;
+//       console.log("result from geocode: " + result);
+//       console.log("addressObj after assignment: " + addressObj);
+//     }).catch(error => {
+//       console.log(error.message);
+//     });
+
+//     addressObj.then(result => {
+//       console.log(result);
+//     });
+
+//     var marker = new google.maps.Marker({
+//       //position: addressObj.results[0].formatted_address,
+//       position: {    lat: 30.6349,
+//         lng: -97.6651},
+//       map: map,
+//       //title: addressObj.results[0].geometry.location
+//     });
+
+//     marker.setMap(map);
+//   })
+// }
