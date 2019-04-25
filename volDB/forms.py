0bcb157fadb9efab908b79437de90ebc57333ccf
S Boyd <boyds@southwestern.edu>
2019-04-25 11:10:15 -0500

    restyle refine search colors

--- volDB/forms.py
+++ volDB/forms.py
@@ -20,13 +20,13 @@ class ResultsPageForm(forms.Form):
     category = forms.ModelChoiceField(queryset=Category.objects.order_by('category').exclude(category='.').exclude(category=''), 
                                             required=False, 
                                             initial=0, 
-                                            widget=forms.Select(attrs={'class' : 'col-sm-3 form-control btn btn-secondary dropdown-toggle',
+                                            widget=forms.Select(attrs={'class' : 'col-sm-3 form-control btn btn-light dropdown-toggle',
                                                                 'type' : 'button', 'data-toggle' : 'dropdown'})
                                             )
     location = forms.ModelChoiceField(queryset=Location.objects.exclude(location='').exclude(location='.'), 
                                     required=False, 
                                     initial=0,
-                                    widget=forms.Select(attrs={'class' : 'col-sm-3 form-control btn btn-secondary dropdown-toggle',
+                                    widget=forms.Select(attrs={'class' : 'col-sm-3 form-control btn btn-light dropdown-toggle',
                                                                 'type' : 'button', 'data-toggle' : 'dropdown'}))
     # Uses Google places API and radius field
     # myLocation = forms.CharField(max_length=100, label='Start Location', widget=forms.TextInput(attrs={'id': 'searchLocation', 'class':'col-sm-3'}), required=False)
