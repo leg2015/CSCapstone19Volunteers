513aca754db454d8530da826f1b439bbb68614e0
ANapForLunch <43053102+ANapForLunch@users.noreply.github.com>
2019-05-01 17:26:58 -0500

    Update upload.py

--- volDB/management/commands/upload.py
+++ volDB/management/commands/upload.py
@@ -7,19 +7,27 @@ class Command(BaseCommand):
     def import_from_csv(self):
          
         print('----- start import method -------')
+        
+        # ------- This file is the CSV file to be uploaded --------
         csvfile = open('UpdatingNPOAaron.csv', encoding="ISO-8859-1")
+        # ---------------------------------------------------------
+        
         print('----- opened csv -------')
         fileReader = csv.reader(csvfile, delimiter='|')
         print('----- csv reader -------', type(fileReader))
         count = 0
         for row in fileReader:
             print('----- row loop -------', row)
+            # Ignores the first row, since it is the header row in the csv
             if count != 0:
+                
+                # Puts in all of the single value fields
                 org, created = Organization.objects.update_or_create(name=row[0], mission=row[1], opportunities=row[4], website=row[8], notes=row[9])
                 
                 org.save()
             
                 # row[2] = categories
+                # Splits the string of categories by ` then adds an entry in the intermediate table for each.
                 categories = str(row[2]).split("`")
                 for cat in categories:
                     catObj, createCat = Category.objects.get_or_create(category=cat)
@@ -29,6 +37,7 @@ class Command(BaseCommand):
                     org.save()
 
                 # row[3] = city/location
+                # Splits the string of locations by ` then adds an entry in the intermediate table for each.
                 location = str(row[3]).split("`")
                 for loc in location:
                     cityObj, createCity = Location.objects.get_or_create(location=loc)
@@ -38,12 +47,15 @@ class Command(BaseCommand):
                     org.save()
 
                 # row[6] = phone number
+                # Splits the string of phone numbers by ` then adds an entry in the intermediate table for each.
                 numbers = str(row[6]).split("`")
                 for num in numbers:
                     newPhone = Phone.objects.create(phone=num, orgID=org)
                     newPhone.save()
                     
                 # row[5] = address
+                # Splits the string of addresses by '`' then splits those strings by ',' then adds an entry in 
+                # the intermediate table for each address, with each substring being a piece of the address.
                 addresses = str(row[5]).split("`")
                 for address in addresses:
                     adr = address.split(",")
@@ -51,6 +63,7 @@ class Command(BaseCommand):
                     newCity = adr[1]
                     newState = adr[2]
                     newZip = adr[3]
+                    # This determines whether or not the address is physical or a PO box.
                     physicalAddress = True
                     if("P.O." in adr[0] or "p.o." in adr[0] or "P.o." in adr[0]):
                         physicalAddress = False
@@ -60,6 +73,7 @@ class Command(BaseCommand):
 
 
                 # row[7] = email
+                # Splits the string of emails by ` then adds an entry in the intermediate table for each.
                 emails = str(row[7]).split("`")
                 for eaddress in emails:
                     newEmail = Email.objects.create(email=eaddress, orgID=org)
